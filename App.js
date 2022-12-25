15908ffe7c8818a13684b83d14c928efdc6ec8fa
Hamees Sayed <hameessayed71@gmail.com>
2022-12-25 13:55:13 +0530

    added light and dark mode

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Main from "./components/Main"
+import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 export default function App() {
-    return (
-        <div className="container">
-            <Navbar />
-            <Main />
-        </div>
-    )
+   const [darkMode, setDarkMode] = React.useState("dark");
+   function toggleDarkMode() {
+      setDarkMode((prevMode) => {
+         if (prevMode === "dark") {
+            return "";
+         } else {
+            return "dark";
+         }
+      });
+   }
+
+   return (
+      <div className="container">
+         <Navbar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+         <Main darkMode={darkMode} />
+      </div>
+   );
 }
